93564cd13ee7f93362a2839576fe8f4bc1d1a74e
andreas-optimizely <andreas@optimizely.com>
2017-10-11 20:51:46 -0700

    trying to fix fonts on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,6 @@ Rails.application.configure do
   config.cache_classes = true
   config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)\z/
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -31,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
