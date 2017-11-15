c04c333acfc1cd7d0d5fde8d1025b635f7bcd06d
Andreas Bloomquist <andreas@optimizely.com>
2017-11-14 18:02:22 -0800

    Revert "Andreas/dynamic fs pid"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,43 +4,34 @@ class ApplicationController < ActionController::Base
 
   include SessionsHelper
   include HTTParty
-  # Optimizely SDK
   require "optimizely"
   require 'uri'
+  
+  uri = URI('https://optimizely.s3.amazonaws.com/json/9010861088.json')
+  datafile = HTTParty.get(uri).body
+  @optimizely_client = Optimizely::Project.new(datafile,Optimizely::EventDispatcher.new,Optimizely::NoOpLogger.new)
 
-  # Optimizely Project ID
-  # Replace this with your own Optimizely Full Stack Project ID
-  PROJECT_ID = '9010861088'
-
-  # Method to handle incoming POST requests from Optimizelys webhook service
-  # When a request is made indicating a change to the datafil we will re-instantiate the Optimizely client
-  # This ensure that the Optimizely client will always be up to sync with changes made in the Optimizely interface
-  def update_optimizely
-    p 'Incoming webhook from Optimizely, instantiating client'
-    instantiate_optimizely
-    head :no_content
+  def optimizely_obj
+    unless @optimizely_client
+      p 'OPTICON DEMO - No Optimizely Object, reinstantiating client'
+      uri = URI('https://optimizely.s3.amazonaws.com/json/9010861088.json')
+      datafile = HTTParty.get(uri).body
+      @optimizely_client = Optimizely::Project.new(datafile, Optimizely::EventDispatcher.new, Optimizely::NoOpLogger.new)
+    end
+    return @optimizely_client
   end
 
+  def update_optimizely
+    p 'OPTICON DEMO - Incoming request from Optimizely webhook'
+    uri = URI('https://optimizely.s3.amazonaws.com/json/9010861088.json')
+    datafile = HTTParty.get(uri).body
+    @optimizely_client = Optimizely::Project.new(datafile, Optimizely::EventDispatcher.new, Optimizely::NoOpLogger.new)
+    return @optimizely_client
+  end
   private
 
-  # Method used to determine if Optimizely has been defined yet
-  def optimizely_obj
-    p 'Checking to see if Optimizely object exists - A16z Event'
-    # Checks to see if the optimizely_client variable has been intialized, if not it will run instantiate_optimizely method
-    # Commenting this out temporarily
-    # @@optimizely_client ||= instantiate_optimizely
-    instantiate_optimizely
-  end
-
-  # Method used to instantiate the Optimizely client if it hasnt been defined or if it needs to be re-instantiated due to changes
-  def instantiate_optimizely
-    p 'Instantiating Client!'
-    # Here we are accessing the datafile directly in S3 to ensure the most up to date datafile
-    # Once the REST API is updated to use the latest (v4) version of the datafile, this will be updated to get the datafile via the API
-    uri = URI("https://optimizely.s3.amazonaws.com/json/#{PROJECT_ID}.json")
-    datafile = HTTParty.get(uri).body
-    # Instantiates Class object that is shared across all controllers
-    @@optimizely_client = Optimizely::Project.new(datafile, Optimizely::EventDispatcher.new, Optimizely::NoOpLogger.new)
+  def set_admin
+    @administration = Administration.first
   end
 
   def set_user
